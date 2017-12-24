187f6a92181a4f720cbaabd895ac3d2ab53050bf
Mark <trandien04@gmail.com>
2017-12-25 00:07:09 +0700

    ADD simple spring boot

---
+++ simple-spring-boot/src/main/java/com/dientm/Application.java
@@ -0,0 +1,21 @@
+package com.dientm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
